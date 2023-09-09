97390c1030e3669fc8b93f0fe70cc152518de0d0
Matt Anikiej <56848215+mattanikiej@users.noreply.github.com>
2023-09-09 13:29:43 -0500

    step through simulation 1000 times

--- simulate.py
+++ simulate.py
@@ -1,4 +1,12 @@
 import pybullet as p
+import time
 
-physicsClient = p.connect(p.GUI)
-p.disconnect()
+physicsClient = p.connect(p.GUI)  # connect client to gui
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)  # disable sidebars to increase performance
+
+for i in range(1000):
+    p.stepSimulation()  # step through simulation
+    time.sleep(1/60)
+    # print(i)
+
+p.disconnect()  # disconnect client
