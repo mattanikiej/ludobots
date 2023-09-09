b25e8cc7a34d63351585f98c3279ce6a6b3a693a
Matt Anikiej <56848215+mattanikiej@users.noreply.github.com>
2023-09-09 14:26:02 -0500

    finished part D

--- generate.py
+++ generate.py
@@ -1,6 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
+x = 0
+y = 0
+z = 0.5
+
 pyrosim.Start_SDF("box.sdf")  # tell pyrosim file where world will be stored
-pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])  # store box in pyrosim
+for i in range(5):
+    for j in range(5):
+        length = 1
+        width = 1
+        height = 1
+        for k in range(5):
+
+            pyrosim.Send_Cube(name="Box", pos=[x+i, y+j, z+k], size=[length, width, height])  # store box in pyrosim
+
+            length *= .9
+            width *= .9
+            height *= .9
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,14 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)  # connect client to gui
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)  # disable sidebars to increase performance
+
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf") # add floor
+
 p.loadSDF("box.sdf")  # read in world from box.sdf
 
 for i in range(1000):
