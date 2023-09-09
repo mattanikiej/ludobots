0e3e54add4a55860d4be46f8b19abc9798cd520d
Matt Anikiej <56848215+mattanikiej@users.noreply.github.com>
2023-09-09 13:50:53 -0500

    finished part B

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)  # connect client to gui
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)  # disable sidebars to increase performance
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)  # disable sidebars to increase performance
 
 for i in range(1000):
     p.stepSimulation()  # step through simulation
